4be5198e55d246762a0849ae6536606caaa35f42
polon59 <przemyslaw.polczak@gmail.com>
2018-09-25 23:05:32 +0200

    Add duck move animation change

--- html/duck.js
+++ html/duck.js
@@ -1,102 +1,45 @@
-var posY = 20;
-var posX = 48;
-
-var firstMove;
-var secondMove;
-
-var destY;
-var destX;
-
-myMove();
 
 
 
-function myMove() {
-    var elem = document.getElementById("duck"); 
-    firstMove = setInterval(first, 10);
 
-    secondMove = setInterval(second, 10);
-    for (let index = 0; index < 4; index++) {
-        pickRandom();
-        // alert(destX + "  " + destY);
-        second();
+
+
+function changeAnimation() {
+    var style = document.documentElement.appendChild(document.createElement("style")),
+    rule = "@keyframes example {\
+        0%   {left: " + getRandom("width") + "%; bottom: 20%;}\
+        10%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        20%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        30%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        40%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        50%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        60%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        70%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        80%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        90%  {left:" + getRandom("width") +"%; bottom:" + getRandom("height") + "%;}\
+        100% {left:" + getRandom("width") +"%; bottom: 100%;}\
+    }";
+    // if (CSSRule.KEYFRAMES_RULE) {
+        style.sheet.insertRule(rule, 0);
+    // } else if (CSSRule.WEBKIT_KEYFRAMES_RULE) { // WebKit
+    //     style.sheet.insertRule("@-webkit-keyframes" + rule, 0);
+    // }
+}
+
+
+function getRandom(valueName) {
+    var randomValue = 0;
+    if (valueName == "width") {
+        randomValue = Math.floor(Math.random() * (90 - 1 + 1)) + 1;
+        // return Math.floor(Math.random() * (95 - 5 + 1)) + 5;
+    } else {
+        randomValue = Math.floor(Math.random() * (80 - 30 + 1)) + 40;
+        // return Math.floor(Math.random() * (80 - 40 + 1)) + 20;
     }
+    console.log(valueName + "  " + randomValue );
+    return randomValue;
+
+    
+}
 
 
-
-        function pickRandom() {
-            destY = Math.random() * (90 - 30) + 30;
-            destX = Math.random() * (99 - 0) + 0;
-        }
-
-        function first() {
-            if (posY >= 50) {
-                clearInterval(firstMove);
-            } else {
-                posX +=0.1; 
-                posY += 0.2;
-                elem.style.bottom = posY + '%';
-                elem.style.left = posX + '%'; 
-            }
-        }
-
-        
-        function second() {
-                if (posY >= destY && posX >= destX) {
-                    clearInterval(secondMove);
-                }
-
-                else {
-                    if (posX < destX && posY < destY) {
-                        posX +=0.1; 
-                        posY += 0.1;
-                        changePosition();
-                    }
-                    else if(posX > destX && posY > destY){
-                        posX -= 0.1; 
-                        posY -= 0.1;
-                        changePosition();
-                    }
-                    else if(posX > destX && posY < destY){
-                        posX -= 0.1; 
-                        posY += 0.1;
-                        changePosition();
-                    }
-                    else if(posX < destX && posY > destY){
-                        posX += 0.1; 
-                        posY -= 0.1;
-                        changePosition();
-                    }
-                    else if(posX == destX && posY > destY){
-                        posY -= 0.1;
-                        changePosition();
-                    }
-                    else if(posX == destX && posY < destY){
-                        posY += 0.1;
-                        changePosition();
-                    }
-                    else if(posX < destX && posY == destY){
-                        posX += 0.1;
-                        changePosition();
-                    }
-                    else if(posX > destX && posY == destY){
-                        posX -= 0.1;
-                        changePosition();
-                    }
-                    else{
-                        alert("CHUJ");
-                    }
-                    
-                }
-            }
-
-        function changePosition() {
-            elem.style.bottom = posY + '%';
-            elem.style.left = posX + '%'; 
-        }
-
-    }
-
-
-
-  
