c8d0a1708d11f47226cea8b3c77547bcbb4a4d81
polon59 <przemyslaw.polczak@gmail.com>
2018-09-27 17:55:55 +0200

    Add dog start-game animations

--- JS/dog.js
+++ JS/dog.js
@@ -1,3 +1,38 @@
-function reload() {
-    changeShootBoxImage(4);
+var dog = document.getElementById("dog");
+dog.addEventListener("animationend",startSniffing)
+
+// walkOut();
+
+// function walkOut() {
+//     dog.addEventListener("animationiteration",startSniffing)
+// }
+
+function hideDog() {
+    dog.style.visibility = "hidden";
+}
+
+function startSniffing() {
+    dog.style.animationName = "dogSniff1";
+    dog.style.backgroundImage = 'url("../resources/sprites/dog/snif.gif")';
+    dog.addEventListener("animationend",startWalk2)
+}
+
+function startWalk2() {
+    dog.style.animationName = "dogWalk2";
+    dog.style.backgroundImage = 'url("../resources/sprites/dog/dogeWalking.gif")';
+    dog.addEventListener("animationend",startSniffing2);
+}
+
+function startSniffing2() {
+    dog.style.animationName = "dogSniff2";
+    dog.style.backgroundImage = 'url("../resources/sprites/dog/snif.gif")';
+    dog.addEventListener("animationend",startJump)
+}
+
+function startJump() {
+    console.log("TO sie kameruje")
+    dog.style.animationDuration = "0.7s";
+    dog.style.animationName = "dogJump";
+    dog.style.backgroundImage = 'url("../resources/sprites/dog/pawelJumper.gif")';
+    dog.addEventListener("animationend", hideDog);
 }
