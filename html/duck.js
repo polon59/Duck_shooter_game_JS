81bd48eec14e2008b4df72d3a51eed9767983462
polon59 <przemyslaw.polczak@gmail.com>
2018-10-03 22:22:32 +0200

    Change changeDirection method to use template strings

--- html/duck.js
+++ html/duck.js
@@ -53,19 +53,19 @@ function flyOutSingleDuck(duck,duckID) {
 
 function changeDirection(duckID) {
     var style = document.documentElement.appendChild(document.createElement("style"));
-    rule = "@keyframes " + duckID + "{\
-        0%   {left: " + getRandomWidth(duckID,30,60) + "%; bottom:" + 20 + "%;}\
-        10%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        20%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        30%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        40%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        50%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        60%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        70%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        80%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        90%  {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,35,70) + "%;}\
-        100% {left:" + getRandomWidth(duckID,1,90) +"%; bottom:" + getRandomHeight(duckID,100,100) + "%;}\
-    }";
+    rule = `@keyframes ${duckID}{
+        0%   {left: ${getRandomWidth(duckID,30,60)}%; bottom: 20%;}
+        10%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        20%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        30%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        40%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        50%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        60%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        70%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        80%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        90%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        100% {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,100,100)}%;}
+    }`;
     style.sheet.insertRule(rule, 0);
     document.getElementById(duckID).style.animationName = duckID;
     document.getElementById(duckID).style.animationDuration = "10s";
@@ -76,7 +76,6 @@ function changeDirection(duckID) {
 function getRandomWidth(duckID,min,max) {
     var generatedWidth = Math.floor(Math.random() * (max - min + 1)) + min;;
     //var widthDifference = generatedWidth - previousWidth;
-
 
     if (generatedWidth < previousWidth){
         duckDirections.push("left");
@@ -92,12 +91,15 @@ function getRandomWidth(duckID,min,max) {
 function getRandomHeight(duckID,min,max) {
     var generatedHeight = Math.floor(Math.random() * (max - min + 1)) + min;
     var heightDifference = generatedHeight - previousHeight;
-    if(duckDirections.length > 3){
-    if (heightDifference > 10){
-        duckDirections[duckDirections.length-1] += "up";
-    } else if (heightDifference < -10) {
-        duckDirections[duckDirections.length-1] += "down";
-    }}
+
+    if (duckDirections.length > 3){
+        if (heightDifference > 10){
+            duckDirections[duckDirections.length-1] += "up";
+        }
+        else if (heightDifference < -10) {
+            duckDirections[duckDirections.length-1] += "down";
+        }
+    }
 
     
     previousHeight = generatedHeight;
@@ -110,7 +112,6 @@ function falldown(duckID,currentWidth,currentHeight) {
     var style = document.documentElement.appendChild(document.createElement("style"));
     var duck = document.getElementById(duckID);
 
-
     rule = `@keyframes ${duckID}fall{
             0%   {left: ${currentWidth} ; top: ${currentHeight};}
             100% {left: ${currentWidth} ; top: 500;}
@@ -118,7 +119,7 @@ function falldown(duckID,currentWidth,currentHeight) {
 
 
     style.sheet.insertRule(rule, 0);
-    duck.style.animationName = duckID + "fall";
+    duck.style.animationName = `${duckID}fall`;
     duck.style.animationDuration = "1s";
     duck.style.backgroundImage = "url('../resources/sprites/duck/falling.gif')";
     duck.style.animationIterationCount = 1;
