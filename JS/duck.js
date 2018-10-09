57b82ee06742d78c2fb681a6072bab1679150446
polon59 <przemyslaw.polczak@gmail.com>
2018-10-09 12:23:04 +0200

    Implement function 'setupFlyAnimation' in duck.js

--- JS/duck.js
+++ JS/duck.js
@@ -1,34 +1,133 @@
-var duck = document.getElementById("duck");
+var animationSpeed = 10;
 
-function myMove() {
-    var elem = document.getElementById("duck"); 
-    var pos = 0;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++; 
-            elem.style.top = pos + 'px'; 
-            elem.style.left = pos + 'px'; 
-        }
+
+function changeAnimationSpeed() {
+    animationSpeed -= 0.5;
+}
+
+
+
+function start() {
+    changeAnimations();
+    resetMagRounds();
+    resetKilledDucksNumber();
+}
+
+
+function changeAnimations() {
+    changeFlyAnimation("duck1");
+    changeFlyAnimation("duck2");
+    resetDuckImage("duck1");
+    resetDuckImage("duck2");
+}
+
+
+function flyOut(duck1, duck2) {
+
+   if (duck1.isAlive) {
+        flyOutSingleDuck(duck1);
+    }
+    if (duck2.isAlive) {
+        flyOutSingleDuck(duck2);
     }
 }
 
-function flyUp() {
 
+function flyOutSingleDuck(duck) {
+    var duckID = duck.id;
+    var duckElement = document.getElementById(duckID);
 
-    freeFlight();
+    var duckCurrentHeight = duckElement.offsetTop;
+    var duckCurrentWidth = duckElement.offsetLeft;
+
+    createFlyOutRule(duckID, duckCurrentWidth, duckCurrentHeight)
+    duckElement.style.animationName = `${duckID}flyout`;
+    duckElement.style.animationDuration = "0.5s";
 }
 
-function freeFlight(){
 
+function createFlyOutRule(duckID, duckCurrentWidth, duckCurrentHeight) {
+    var style = document.documentElement.appendChild(document.createElement("style"));
+
+    rule = `@keyframes ${duckID}flyout{
+        0%   {left: ${duckCurrentWidth}; top: ${duckCurrentHeight};}
+        100% {left: ${duckCurrentWidth}; top: 0%;}
+    }`;
+
+    style.sheet.insertRule(rule, 0);
 }
 
-function escape(){
 
+
+function changeFlyAnimation(duckID) {
+    var duck = document.getElementById(duckID);
+    var style = document.documentElement.appendChild(document.createElement("style"));
+    var rule = createRule(duckID);
+
+    style.sheet.insertRule(rule, 0);
+    setupFlyAnimation(duck, duckID);
 }
 
-function shot(){
-    "Nie w szczepionke chuju!";
+
+function setupFlyAnimation(duck, duckID) {
+    duck.style.animationName = duckID;
+    duck.style.animationDuration = `${animationSpeed}s`;
+    duck.style.animationIterationCount = "infinite";
+}
+
+
+function createRule(duckID) {
+    rule = `@keyframes ${duckID}{
+        0%   {left: ${getRandomWidth(duckID,30,60)}%; bottom: 20%;}
+        10%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        20%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        30%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        40%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        50%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        60%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        70%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        80%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        90%  {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,35,85)}%;}
+        100% {left: ${getRandomWidth(duckID,10,90)}%; bottom: ${getRandomHeight(duckID,100,100)}%;}
+    }`;
+    return rule;
+}
+
+
+function getRandomWidth(duckID,min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+function getRandomHeight(duckID,min,max) {
+    return generatedHeight = Math.floor(Math.random() * (max - min + 1)) + min;   
+}
+
+
+function falldown(duckID,currentWidth,currentHeight) {
+    var duck = document.getElementById(duckID);
+
+    createFalldownRule(duckID, currentWidth, currentHeight) 
+    duck.style.animationName = `${duckID}fall`;
+    duck.style.animationDuration = "0.5s";
+    duck.style.backgroundImage = "url('../resources/sprites/duck/falling.gif')";
+    duck.style.animationIterationCount = 1;
+}
+
+
+function createFalldownRule(duckID, currentWidth, currentHeight) {
+    var style = document.documentElement.appendChild(document.createElement("style"));
+
+    rule = `@keyframes ${duckID}fall{
+        0%   {left: ${currentWidth} ; top: ${currentHeight};}
+        100% {left: ${currentWidth} ; top: 500;}
+        }`;
+
+    style.sheet.insertRule(rule, 0);
+}
+
+
+function resetDuckImage(duckID){
+    var duck = document.getElementById(duckID);
+    duck.style.backgroundImage = "url('../resources/sprites/duck/right1.png')";
 }
