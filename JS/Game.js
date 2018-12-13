83a9040fdbc92d30e3bf3695490208baf5d0606f
polon59 <przemyslaw.polczak@gmail.com>
2018-12-13 22:39:10 +0100

    Add methods to create multiple ducks during gameplay

--- JS/Game.js
+++ JS/Game.js
@@ -3,40 +3,54 @@ class Game{
     constructor(){
         this.dog1 = new Dog("dog1");
         this.dog2 = new Dog("dog2");
+        this.ducks = this.createDucks(15);
         this.duck1 = new Duck("duck1");
         this.duck2 = new Duck("duck2");
-        this.shotHandler = new ShotHandler(this);
+        this.shotHandler = new ShotHandler();
     }
 
     startGame(){
         document.getElementById("startScreen").style.display = "none";
         // this.dog1.launchWalkoutAnimation();
         // setTimeout(() => this.startDucksFlight(), 7300);
+        this.startNewRound();
+        $("#sky").click(this.shoot.bind(this));
 
+        
+    }
+
+    shoot(){
+        this.shotHandler.checkIfHitSuccessful(this.duck1, this.duck2);
+    }
+
+    startNewRound(){
         this.startDucksFlight();
         this.shotHandler.enableShooting();
-        document.onkeydown = this.checkKey.bind(this);
     }
 
 
     startDucksFlight(){
-        this.duck1.startFlight();
-        this.duck2.startFlight();
+        this.ducks.forEach(duck => {
+            duck.startFlight();
+        });
+
+        // this.duck1.startFlight();
+        // this.duck2.startFlight();
+        // console.log(this.duck1.currentWidth);
 
     }
 
 
-    checkKey(e){
+    createDucks(numberOfDucks){
+        let ducksList = new Array();
 
-        e = e || window.event;
-        let pressedKeyCode = e.keyCode;
+        for (let id = 1; id < numberOfDucks+1; id++) {
+            let currentId = `duck${id}`;
+            ducksList.push(new Duck(currentId));
+            $("#sky").append(`<div id="${currentId}" class="duck"></div>`);
+        }
 
-        console.log("STOP");
-        
-        if (pressedKeyCode === 13) {
-            this.duck1.fallDown();
-            this.duck2.fallDown();
-        } 
+        return ducksList;
     }
 
 
--- JS/ShotHandler.js
+++ JS/ShotHandler.js
@@ -1,18 +1,56 @@
 class ShotHandler{
 
-    constructor(game_){
-        debugger
+    constructor(){
         $("#sky").click(this.shot);
-        this.game = game_;
     }
 
-
-    shot(){
-        alert("BANG");
-        this.game.duck1.fallDown();
+    checkIfHitSuccessful(duck1, duck2){
+        duck1.fallDown();
+        duck2.fallDown();
     }
 
     enableShooting(){
         $("#shootBlocker").hide();
     }
+
+
+    // subtractShoots() {
+    //     var mouseX = event.clientX;
+    //     var mouseY = event.clientY;
+    //     var duck1x = duck1element.offsetLeft;
+    //     var duck1y = duck1element.offsetTop;
+    //     var duck2x = duck2element.offsetLeft;
+    //     var duck2y = duck2element.offsetTop;
+    
+    //     if (checkIfHit(mouseX,mouseY,duck1x,duck1y)) {
+    //         duck1.isAlive = false;
+    //         falldown("duck1",duck1x,duck1y); 
+    //     }
+    //     else if(checkIfHit(mouseX,mouseY,duck2x,duck2y)){
+    //         duck2.isAlive = false;
+    //         falldown("duck2",duck2x,duck2y);
+    //     }
+    //     else{
+    //         playSound("miss");
+    //         changeHitBoxImage("miss");
+    //     }
+    // }
+    
+    
+    // checkIfHit(mouseX,mouseY,duckX,duckY) {
+    //     let duckWidth = 78;
+    //     let duckHeight = 73;
+    
+    //     if ((mouseX>=duckX) && (mouseX <= duckX+duckHeight) && 
+    //     (mouseY >= duckY) && (mouseY <= duckY+duckWidth)){
+    //         changeHitBoxImage("hit");        
+    //         playSound("hit");
+    //         killedDucks++;
+    //         addPoints();
+    //         return true;
+    //     }
+    //     else{
+    //         return false;
+    //     }
+    // }
 }
