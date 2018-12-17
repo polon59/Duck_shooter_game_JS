7e3c970858803ab8ba06fd7226ce5cd80104986c
polon59 <przemyslaw.polczak@gmail.com>
2018-12-17 23:50:38 +0100

    Implement method 'changeVisibilityOfArrows' in class 'StartScreen'

--- JS/StartScreen.js
+++ JS/StartScreen.js
@@ -5,14 +5,15 @@ class StartScreen{
         this.currentModeIndex = 0;
         this.initializeModes();
         this.initializeButtons();
+        this.changeMode("next");
     }
 
 
     initializeModes(){
         this.availableModes.push(
-            {name:"NORMAL", difficulty:"NORMAL", ammunition:3, ducks:2},
             {name:"CUSTOM", difficulty:"EASY", ammunition:5, ducks:2},
-            {name:"EXTREME", difficulty:"HARD", ammunition:30, ducks:15},
+            {name:"CLASSIC", difficulty:"EASY", ammunition:3, ducks:2},
+            {name:"EXTREME", difficulty:"HARD", ammunition:30, ducks:15}
         )
     }
 
@@ -23,8 +24,8 @@ class StartScreen{
     }
 
 
-    changeMode(value){
-        if (value == "next") {
+    changeMode(togle){
+        if (togle == "next") {
             if (this.currentModeIndex<2) {
                 this.currentModeIndex++;
             }
@@ -34,19 +35,41 @@ class StartScreen{
                 this.currentModeIndex--;
             }
         }
-        this.displaySettingsForSelectedMode();
+        this.displaySettingsForCurrentMode();
     }
 
 
-    displaySettingsForSelectedMode(){
+    displaySettingsForCurrentMode(){
         let selectedMode = this.availableModes[this.currentModeIndex];
 
         $("#modeSelect .selection").html(selectedMode.name);
         $("#diffSelect .selection").html(selectedMode.difficulty);
         $("#ammoSelect .selection").html(selectedMode.ammunition);
         $("#ducksSelect .selection").html(selectedMode.ducks);
+
+        this.changeVisibilityOfArrows();
     }
 
+
+
+    changeVisibilityOfArrows(){
+        let currentModeName = this.availableModes[this.currentModeIndex].name;
+        if (currentModeName == "CLASSIC") {
+            $("#diffSelect .arrow").css("visibility","visible");
+            $("#ammoSelect .arrow").css("visibility","hidden");
+            $("#ducksSelect .arrow").css("visibility","hidden");
+        }
+        else if(currentModeName == "CUSTOM"){
+            $("#diffSelect .arrow").css("visibility","visible");
+            $("#ammoSelect .arrow").css("visibility","visible");
+            $("#ducksSelect .arrow").css("visibility","visible");
+        }
+        else{
+            $("#diffSelect .arrow").css("visibility","hidden");
+            $("#ammoSelect .arrow").css("visibility","hidden");
+            $("#ducksSelect .arrow").css("visibility","hidden");
+        }
+    }
 
 
 
